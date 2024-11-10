965efb5505a4255dae30f7b99f8eb2c4f40724c8
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2024-11-10 21:40:47 +0700

    Update settings.py

--- exam_monitoring/settings.py
+++ exam_monitoring/settings.py
@@ -28,13 +28,13 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['https://web-production-4f5a7.up.railway.app/', 'web-production-4f5a7.up.railway.app', 'https://thitructuyenai.online/', 'thitructuyenai.online']
+ALLOWED_HOSTS = ['https://web-production-f978b.up.railway.app']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-4f5a7.up.railway.app/', 'web-production-4f5a7.up.railway.app', 'https://thitructuyenai.online/', 'thitructuyenai.online']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-f978b.up.railway.app']
 
 # Cors settings 
 CORS_ALLOW_ALL_ORIGINS = True 
